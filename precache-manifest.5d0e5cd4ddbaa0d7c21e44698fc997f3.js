d285902a9ac1b062f32170f6e49b11229d8d3808
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-10-02 01:22:44 +0530

    Updates

---
+++ precache-manifest.5d0e5cd4ddbaa0d7c21e44698fc997f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f51250000b33949f62a",
+    "url": "/static/css/main.f4fdc808.chunk.css"
+  },
+  {
+    "revision": "6f51250000b33949f62a",
+    "url": "/static/js/main.6f512500.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "625cc90102493a7439cc",
+    "url": "/static/css/2.076b11e0.chunk.css"
+  },
+  {
+    "revision": "625cc90102493a7439cc",
+    "url": "/static/js/2.625cc901.chunk.js"
+  },
+  {
+    "revision": "a5627a9c6f1e58df88af",
+    "url": "/static/js/runtime~main.a5627a9c.js"
+  },
+  {
+    "revision": "bf60445e87719910bcf6cf188e1142ee",
+    "url": "/static/media/securemeters.bf60445e.jpg"
+  },
+  {
+    "revision": "0e034765b0c91d3cf6a1bd43cadcb0af",
+    "url": "/index.html"
+  }
+];
